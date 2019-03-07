a70117c4ad1e38869ecd63a72836e57f7bd1ebba
Mitra Pantic <mitra196506@gmail.com>
2019-03-07 21:05:35 +0100

    Kreirane nove aktivnosti

---
+++ DoPostEmail/app/src/main/java/com/example/dopostemail/activities/CreateFolderActivity.java
@@ -0,0 +1,41 @@
+package com.example.dopostemail.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.dopostemail.R;
+
+public class CreateFolderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_folder);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+}

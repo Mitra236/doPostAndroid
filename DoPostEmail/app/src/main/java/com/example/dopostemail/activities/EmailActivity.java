52faebbffccba129151affa2e4a2a82d4f24497a
Emilija Drazic <ema.emilija98@gmail.com>
2019-03-07 21:58:13 +0100

    Kreirane nove aktivnosti

---
+++ DoPostEmail/app/src/main/java/com/example/dopostemail/activities/EmailActivity.java
@@ -0,0 +1,38 @@
+package com.example.dopostemail.activities;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.dopostemail.R;
+
+public class EmailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+}

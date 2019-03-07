aadfe8242cb2f2b33de57d26947635cbc42667bf
scavenger78 <aleksandar.budimirovic97@gmail.com>
2019-03-07 21:56:01 +0100

    dodata zadnja 4 activitiJA

---
+++ DoPostEmail/app/src/main/java/com/example/dopostemail/activities/ContactsActivity.java
@@ -0,0 +1,41 @@
+package com.example.dopostemail.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.dopostemail.R;
+
+public class ContactsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+
+}

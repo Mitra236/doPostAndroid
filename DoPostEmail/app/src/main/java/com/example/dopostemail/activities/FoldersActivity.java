4ddc16da21e1036713cf7a2d4941ff5ffffd23ae
Emilija Drazic <ema.emilija98@gmail.com>
2019-03-30 23:06:27 +0100

    Uradjen navigation drawer za folders

--- DoPostEmail/app/src/main/java/com/example/dopostemail/activities/FoldersActivity.java
+++ DoPostEmail/app/src/main/java/com/example/dopostemail/activities/FoldersActivity.java
@@ -1,16 +1,65 @@
 package com.example.dopostemail.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.example.dopostemail.R;
 
-public class FoldersActivity extends AppCompatActivity {
+public class FoldersActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Folders");
         setContentView(R.layout.activity_folders);
+
+        Toolbar toolbar = findViewById(R.id.nav_toolbar_folders);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        NavigationView navView = findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(this);
+
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_open);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch(menuItem.getItemId()){
+            case R.id.nav_emails:
+                Intent i = new Intent(FoldersActivity.this, EmailsActivity.class);
+                startActivity(i);
+                break;
+            case R.id.nav_contacts:
+                Intent j = new Intent(FoldersActivity.this, ContactsActivity.class);
+                startActivity(j);
+                break;
+            case R.id.nav_folders:
+                Intent k = new Intent(FoldersActivity.this, FoldersActivity.class);
+                startActivity(k);
+                break;
+            case R.id.nav_settings:
+                Intent l = new Intent(FoldersActivity.this, SettingsActivity.class);
+                startActivity(l);
+                break;
+        }
+
+
+        return true;
     }
 
     @Override

89debea61274211f82cbf5f207efb5ab3eda55c2
Mitra Pantic <mitra196506@gmail.com>
2019-04-12 18:21:49 +0200

    Dodate ikonice i toast poruke

--- DoPostEmail/app/src/main/java/com/example/dopostemail/activities/CreateEmailActivity.java
+++ DoPostEmail/app/src/main/java/com/example/dopostemail/activities/CreateEmailActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.ViewUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.dopostemail.R;
 
@@ -13,6 +16,7 @@ public class CreateEmailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_email);
+        setTitle("Compose");
 
         Toolbar toolbar = findViewById(R.id.toolbar_create_emails);
         setSupportActionBar(toolbar);
@@ -21,6 +25,28 @@ public class CreateEmailActivity extends AppCompatActivity {
 
         Utils.darkenStatusBar(this, R.color.colorToolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_send, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_send:
+                Toast.makeText(getApplicationContext(), "Message sent", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_cancel:
+                Toast.makeText(getApplicationContext(), "Canceled", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 
     @Override
     protected void onStart(){
--- DoPostEmail/app/src/main/java/com/example/dopostemail/activities/EmailActivity.java
+++ DoPostEmail/app/src/main/java/com/example/dopostemail/activities/EmailActivity.java
@@ -3,6 +3,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
 import com.example.dopostemail.R;
@@ -27,6 +29,30 @@ public class EmailActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_items, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_replay:
+                Toast.makeText(getApplicationContext(), "Replied", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_delete:
+                Toast.makeText(getApplicationContext(), "Message deleted", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_replay_toAll:
+                Toast.makeText(getApplicationContext(), "Replied to all", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_attach:
+                Toast.makeText(getApplicationContext(), "Files Attached", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_forward:
+                Toast.makeText(getApplicationContext(), "Forwarded", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override

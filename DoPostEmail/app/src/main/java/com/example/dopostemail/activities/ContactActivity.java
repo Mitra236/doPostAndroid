eb944fbcbcc9ffec1042d349961eea73ca3e2748
Aleksandar Budimirovic <aleksandar.budimirovic97@gmail.com>
2019-04-15 15:39:46 +0200

    dodat toast na toolbar dugmice u ContactActivity

--- DoPostEmail/app/src/main/java/com/example/dopostemail/activities/ContactActivity.java
+++ DoPostEmail/app/src/main/java/com/example/dopostemail/activities/ContactActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.dopostemail.R;
 import com.example.dopostemail.model.Contact;
@@ -52,6 +55,25 @@ public class ContactActivity extends AppCompatActivity {
     @Override
     protected void onResume(){
         super.onResume();
+
+        Button save = (Button)findViewById(R.id.button_save_c);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ContactActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        Button back = (Button)findViewById(R.id.button_back_c);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ContactActivity.this, "Back", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
 
     }
 

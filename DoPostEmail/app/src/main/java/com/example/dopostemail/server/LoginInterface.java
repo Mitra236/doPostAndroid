9643211d96ed788d450fdf3beff601ce69c227ea
Mitra Pantic <mitra196506@gmail.com>
2019-05-08 22:52:01 +0200

    Izmenjen shared prefrences u login activity

--- DoPostEmail/app/src/main/java/com/example/dopostemail/server/LoginInterface.java
+++ DoPostEmail/app/src/main/java/com/example/dopostemail/server/LoginInterface.java
@@ -5,10 +5,14 @@ import com.example.dopostemail.model.Account;
 import java.util.ArrayList;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 
 public interface LoginInterface {
 
     @GET("accounts")
     Call<ArrayList<Account>> getAccounts();
+
+    @GET("editAccount")
+    Call<Account> editAccount(@Body Account acc);
 }
